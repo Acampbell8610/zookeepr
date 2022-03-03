4000108457bad619b39fe3388abd359504f24468
acamp <acampbell8610@gmail.com>
2022-03-02 21:07:27 -0600

    zookeeper link to front end

--- server.js
+++ server.js
@@ -10,9 +10,11 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
+
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
-app.use(express.static('public'));
+
 
 
 
